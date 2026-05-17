8977741ea0379a87a9fb4ff21ce3ac39efb01af5
Krishnajith Subramanian <krishnajiths2009@gmail.com>
2026-05-17 12:31:01 -0400

    Implement name check for secret access
    Add authorization check for accessing the secret message.

--- scripts/conditional_script.sh
+++ scripts/conditional_script.sh
@@ -2,11 +2,17 @@
 
 echo "Please enter your name first:"
 read name
+
+# reads the name variable to see if it matches the authorized use
 if [ "$name" = "Krishnajith" ]; then
   echo "Welcome Krishnajith! Here is the secret: You are a cool guy!"
+  
+# if the first case fails this is what will be displayed
 else 
   "Sorry! You are not authorized to access the secret."
+  
+# signals that the conditional is over
 fi
 
 # The conditional script uses an if statement to read the name variable to make sure only the authorized individual can see the secret
-# It is space sensitive | fi signals that the conditional is over
+
